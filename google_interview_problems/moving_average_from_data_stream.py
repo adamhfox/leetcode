faaf55af4ddefec0d17f50fd17a053c1e01a4bdb
adamhfox <adamhfox624@gmail.com>
2020-08-13 12:35:30 -0400

    Problem Solution

--- google_interview_problems/moving_average_from_data_stream.py
+++ google_interview_problems/moving_average_from_data_stream.py
@@ -7,3 +7,19 @@ m.next(10) = (1 + 10) / 2
 m.next(3) = (1 + 10 + 3) / 3
 m.next(5) = (10 + 3 + 5) / 3
 '''
+class MovingAverage:
+
+    def __init__(self, size: int):
+        """
+        Initialize your data structure here.
+        """
+        self.size = size
+        self.window = []
+
+    def next(self, val: int) -> float:
+        self.window.insert(0, val)
+        
+        if len(self.window) > self.size:
+            self.window.pop()
+            
+        return sum(self.window) / len(self.window)
